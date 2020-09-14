0eeec38311a95ec24e6013e427a3cc5b5947d781
Michael Eclavea <usermike671@gmail.com>
2020-09-14 15:55:16 -0700

    fixed city

--- server.js
+++ server.js
@@ -3,27 +3,42 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { response } = require('express');
 app.use(cors());
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+app.get('/', function (request, response) {
+    response.send('Hello World');
+  });
+
+app.get('/location', handlerLocation);
+
+function Location(place, city){
+    this.search_query = place;
+    this.formatted_query = city.display_name; 
+    this.lattitude = city.lat; 
+    this.longitude = city.lon;
+}
 
 
 
-// app.get('/location', handlerLocation);
-
-
-// function handlerLocation(req, res) {
-//     try {
-//         let place = req.query.display_name;
-//     } catch(error) {
-//         response.status(500).send('Broken request');
-//     }
-// }
-
-
+function handlerLocation(request, response) {
+   
+    try {
+        const place = request.query.city;
+        let city = require('./data/location.json');
+        console.log(city);
+        let locationData = new Location(place, city[0]); 
+        response.status(200).json(locationData);
+    } catch(error) {
+        response.status(500).send('Broken request');
+    }
+   
+}
 
 
 app.listen(PORT, () => {
-    console.log('listening on PORT 3000');
+    console.log(`listening on port ${PORT}`);
 });
+
